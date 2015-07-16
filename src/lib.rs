5e3522f9705e453168597009406c5367e21b61a0
Markus Heikkinen <ittevien@gmail.com>
2015-07-17 00:01:05 +0300

    Add basic hooking functionality and nightly update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(libc, link_args)]
+#![feature(libc, link_args, wrapping, asm)]
 #[link_args = "-static-libgcc"]
 extern {}
 extern crate libc;
@@ -6,7 +6,8 @@ extern crate kernel32;
 extern crate winapi;
 
 use PatchHistory::*;
-use std::mem;
+use std::{ops, mem};
+use std::marker::PhantomData;
 
 mod platform;
 
@@ -17,21 +18,24 @@ enum PatchHistory {
 
 pub struct PatchManager {
     history: Vec<PatchHistory>,
+    exec_heap: platform::ExecutableHeap,
 }
 
 impl PatchManager {
     pub fn new() -> PatchManager {
         PatchManager {
             history: Vec::new(),
+            exec_heap: platform::ExecutableHeap::new(),
         }
     }
-    pub unsafe fn patch_exe(&mut self) -> Patch {
+    pub unsafe fn patch_exe<P: FnMut(&mut Patch)>(&mut self, mut closure: P) {
         let exe_addr = platform::exe_addr();
-        Patch {
+        let mut patch = Patch {
             parent: self,
             _protection: platform::MemoryProtection::new(exe_addr),
             diff: 0,
-        }
+        };
+        closure(&mut patch);
     }
 }
 
@@ -50,7 +54,7 @@ impl<'a> Patch<'a> {
         self.parent.history.push(Replace32(ptr, val));
         *ptr = val;
     }
-    pub unsafe fn replace<Addr:ToPointer>(&mut self, addr: &Addr, data: Vec<u8>) {
+    pub unsafe fn replace<Addr: ToPointer>(&mut self, addr: &Addr, data: Vec<u8>) {
         let ptr = addr.ptr().offset(self.diff);
         let mut i = 0;
         for byte in data.iter() {
@@ -58,6 +62,55 @@ impl<'a> Patch<'a> {
             i += 1;
         }
         self.parent.history.push(Replace(ptr, data));
+    }
+
+    pub unsafe fn hook<Hook>(&mut self, _hook: Hook, target: Hook::Target) where Hook: HookableAsmWrap {
+        let data = self.make_hook_wrapper::<Hook>(target);
+        platform::jump_hook(Hook::address(), mem::transmute(data));
+    }
+
+    unsafe fn make_hook_wrapper<Hook>(&mut self, target: Hook::Target) -> *mut u8 where
+        Hook: HookableAsmWrap
+    {
+        let mut hook_ptr: *mut u8 = mem::transmute(&target);
+        let mut code = Hook::get_hook_wrapper();
+        let mut code_size = 0;
+        while *code != 0xcc { code = code.offset(1); }
+        code = code.offset(1);
+        let mut hook_pos = code;
+        while *hook_pos != 0xcc { hook_pos = hook_pos.offset(1); code_size += 1; }
+        let mut code_end = hook_pos;
+        while *code_end == 0xcc { code_end = code_end.offset(1); code_size += 1; }
+        while *code_end != 0xcc { code_end = code_end.offset(1); code_size += 1; }
+        let data = self.alloc_exec(code_size);
+        let mut data_pos = data;
+
+        while code != hook_pos { 
+            *data_pos = *code;
+            code = code.offset(1);
+            data_pos = data_pos.offset(1);
+        }
+        while *code == 0xcc {
+            *data_pos = *hook_ptr;
+            hook_ptr = hook_ptr.offset(1);
+            code = code.offset(1);
+            data_pos = data_pos.offset(1);
+        }
+        while code != code_end { 
+            *data_pos = *code;
+            code = code.offset(1);
+            data_pos = data_pos.offset(1);
+        }
+        data
+    }
+
+    pub unsafe fn call_hook<Hook>(&mut self, _hook: Hook, target: Hook::Target) where Hook: HookableAsmWrap {
+        let data = self.make_hook_wrapper::<Hook>(target);
+        platform::call_hook(Hook::address(), mem::transmute(data), &mut self.parent.exec_heap);
+    }
+
+    fn alloc_exec(&mut self, size: usize) -> *mut u8 {
+        self.parent.exec_heap.allocate(size)
     }
 }
 
@@ -75,4 +128,45 @@ impl ToPointer for usize {
 /// Accepts only ascii strings for filename to avoid unicode dependency.
 pub unsafe fn redirect_stderr(filename: &str) -> bool {
     platform::redirect_stderr(filename)
+}
+
+pub trait HookableAsmWrap {
+    type Target;
+    unsafe fn get_hook_wrapper() -> *const u8;
+    fn address() -> usize;
+}
+
+pub struct Variable<T> {
+    pub address: usize,
+    pub phantom: PhantomData<T>,
+}
+
+unsafe impl<T> Sync for Variable<T> { }
+
+impl<T> Variable<T> {
+    pub fn new(addr: usize) -> Variable<T> {
+        Variable {
+            address: addr,
+            phantom: PhantomData,
+        }
+    }
+    pub unsafe fn ptr(&self) -> *const T {
+        mem::transmute(self.address)
+    }
+    pub unsafe fn mut_ptr(&self) -> *mut T {
+        mem::transmute(self.address)
+    }
+}
+
+impl<T> ops::Deref for Variable<T> {
+    type Target = T;
+    fn deref<'a>(&'a self) -> &'a T {
+        unsafe { mem::transmute(self.address) }
+    }
+}
+
+impl<T> ops::DerefMut for Variable<T> {
+    fn deref_mut<'a>(&'a mut self) -> &'a mut T {
+        unsafe { mem::transmute(self.address) }
+    }
 }
--- src/platform.rs
+++ src/platform.rs
@@ -3,10 +3,12 @@ use libc::types::common::c95::c_void;
 use libc::consts::os::extra::{PAGE_EXECUTE_READWRITE, GENERIC_WRITE, FILE_SHARE_READ, FILE_ATTRIBUTE_NORMAL};
 use libc::consts::os::extra::{CREATE_ALWAYS};
 use libc::funcs::extra::kernel32::{VirtualProtect, VirtualQuery};
-use kernel32::{GetModuleHandleW, CreateFileA, SetStdHandle};
-use winapi::winbase::STD_ERROR_HANDLE;
+use kernel32::{GetModuleHandleW, CreateFileA, SetStdHandle, HeapCreate, HeapAlloc};
+use winapi::{HANDLE, STD_ERROR_HANDLE};
 
 use std::{mem, ptr};
+use std::num::wrapping::OverflowingOps;
+use ::ToPointer;
 
 pub fn nop() -> u8 {
     0x90
@@ -63,5 +65,108 @@ pub unsafe fn redirect_stderr(filename: &str) -> bool {
         SetStdHandle(STD_ERROR_HANDLE, handle) != 0
     } else {
         false
+    }
+}
+
+pub struct ExecutableHeap {
+    handle: HANDLE,
+}
+
+impl ExecutableHeap {
+    pub fn new() -> ExecutableHeap {
+        let options = 0x00040000;
+        ExecutableHeap {
+            handle: unsafe { HeapCreate(options, 0, 0) },
+        }
+    }
+    pub fn allocate(&mut self, size: usize) -> *mut u8 {
+        unsafe { mem::transmute(HeapAlloc(self.handle, 0, size as u32)) }
+    }
+}
+
+pub unsafe fn jump_hook<T: ToPointer>(src: T, tgt: *const u8) {
+    let mut ptr = src.ptr();
+    *ptr = 0xe9;
+    ptr = ptr.offset(1);
+    let val: usize = mem::transmute::<_, usize>(tgt).overflowing_sub(mem::transmute::<_, usize>(ptr) + 4).0;
+    let hook_ptr: *mut usize = mem::transmute(ptr);
+    *hook_ptr = val;
+}
+
+unsafe fn x86_sib_ins_size(ins: *const u8) -> usize {
+    let sib = if (*ins.offset(1) & 0x7) == 0x4 { 1 } else { 0 };
+    match (*ins.offset(1) & 0xc0) >> 6 {
+        0 => if (*ins.offset(1) & 0x7) == 0x5 { 6 + sib } else { 2 + sib },
+        1 => 3 + sib,
+        2 => 6 + sib,
+        _ => 2, // No sib
+    }
+}
+
+unsafe fn x86_ins_size(ins: *const u8) -> usize {
+    match *ins {
+        push_pop if push_pop >= 0x50 && push_pop < 0x62 => 1,
+        n if n >= 0x84 && n < 0x90 => x86_sib_ins_size(ins),
+        0x83 | 0xc6 => x86_sib_ins_size(ins) + 1,
+        n => panic!("Unimpl ins size {}", n),
+    }
+}
+
+unsafe fn x86_copy_instructions(mut src: *const u8, mut dst: *mut u8, mut len: isize) {
+    while len > 0 {
+        let ins_len = x86_ins_size(src) as isize;
+        // Relative jumps need to be handled differently
+        match *src {
+            _ => ptr::copy_nonoverlapping(src, dst, ins_len as usize),
+        }
+        src = src.offset(ins_len);
+        dst = dst.offset(ins_len);
+        len -= ins_len;
+    }
+}
+
+pub unsafe fn call_hook<T: ToPointer>(src: T, tgt: *const u8, heap: &mut ExecutableHeap) {
+    let mut ptr = src.ptr();
+    let mut wrapper_size = 0isize;
+    while wrapper_size < 5 {
+        wrapper_size += x86_ins_size(ptr.offset(wrapper_size)) as isize;
+    }
+    let wrapper = heap.allocate(1 + 5 + wrapper_size as usize + 2);
+    *wrapper.offset(0) = 0x60; // Pushad
+    *wrapper.offset(1 + 5) = 0x61; // Popad
+    *wrapper.offset(1 + 5 + 1 + wrapper_size) = 0xc3; // Retn
+    x86_copy_instructions(ptr, wrapper.offset(1 + 5 + 1), wrapper_size);
+    for i in 5..wrapper_size {
+        *ptr.offset(i) = nop();
+    }
+    *ptr = 0xe8;
+    ptr = ptr.offset(1);
+    let val: usize = mem::transmute::<_, usize>(wrapper).overflowing_sub(mem::transmute::<_, usize>(ptr) + 4).0;
+    let hook_ptr: *mut usize = mem::transmute(ptr);
+    *hook_ptr = val;
+    *wrapper.offset(1) = 0xe8;
+    let val: usize = mem::transmute::<_, usize>(tgt).overflowing_sub(mem::transmute::<_, usize>(wrapper) + 6).0;
+    let hook_ptr: *mut usize = mem::transmute(wrapper.offset(2));
+    *hook_ptr = val;
+}
+
+#[test]
+fn test_x86_ins_size() {
+    let inss = vec![vec![0x89, 0x45, 0xfc], vec![0xc6, 0x05, 0xfb, 0x01, 0xe8, 0xdc, 0xfa],
+    vec![0xc6, 0x45, 0xfb, 0x01], vec![0x55], vec![0x60], vec![0x8b, 0xec], vec![0x83, 0xec, 0x0c]];
+    unsafe {
+        for ins in inss {
+            assert_eq!(ins.len(), x86_ins_size(ins.as_ptr()));
+        }
+    }
+}
+
+#[test]
+fn test_x86_copy_ins() {
+    let ins1 = vec![0x89, 0x45, 0xfc, 0xff];
+    unsafe {
+        let mut tgt = vec![0; 5];
+        x86_copy_instructions(ins1.as_ptr(), tgt.as_mut_ptr(), 1);
+        assert_eq!(tgt, vec![0x89, 0x45, 0xfc, 0x00, 0x00]);
     }
 }
